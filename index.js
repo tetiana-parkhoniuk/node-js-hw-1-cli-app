9adb1e04c950535a4f0ab77e4ff77247faba9a4f
Tetiana Parkhoniuk <tetiana.parkhoniuk@gmail.com>
2021-09-15 19:39:35 +0300

    Adds contacts.js and listContacts.
    Step 2 in progress.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hi Node.js!");
+const contactsOperations = require("./contacts");
+
+const workWithContacts = async (type = "listContacts", id, data) => {
+    try {
+        switch (type) {
+            case "listContacts":
+                return await contactsOperations.listContacts();
+        }
+    } catch (error) {
+             throw new error;
+    }
+};
+
+workWithContacts("getAll")
+    .then(data => console.log(data))
+    .catch(error => console.log(error))
